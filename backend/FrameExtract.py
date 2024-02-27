8c166a44def394796a3b3233752fc090b1cdd4be
TharushaMaddumage <w1998721@my.westminster.ac.uk>
2024-02-27 15:20:32 +0530

    Code for frame extraction from live video feed.

---
+++ backend/FrameExtract.py
@@ -0,0 +1,17 @@
+### CeraFlaw - FrameExtract.py
+
+import os
+
+# OpenCv lib
+import cv2
+
+# Capture obj
+vidCam = cv2.VideoCapture(0)
+
+# Variables
+currentFrame = 0
+
+# Savedata directory
+savedata_dir = "backend//Frames"
+#savedata_dir = os.path.expanduser("~\\Desktop\\FrameExtract\\Frames")
+os.makedirs(savedata_dir, exist_ok=True)
