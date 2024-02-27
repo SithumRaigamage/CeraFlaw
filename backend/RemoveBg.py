63e17ddc7ca789f986c379c5174c70c4a320c94f
Praveen Dileesha <dileeshapraveen@gmail.com>
2024-02-27 13:00:17 +0530

    added the background remover

---
+++ backend/RemoveBg.py
@@ -0,0 +1,31 @@
+from rembg import remove
+from PIL import Image
+import os
+
+folder_path = "original_tile_frames"
+output_folder = "noBG_tile_frames"
+
+# Get a list of all files in the folder
+file_list = os.listdir(folder_path)
+
+# Filter only image files (you can customize the extension as needed)
+image_files = [file for file in file_list if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp'))]
+
+for image_file in image_files:
+    # Construct the full path to the image file
+    image_path = os.path.join(folder_path, image_file)
+
+    img = Image.open(image_path)
+    #using remove method
+    output_image = remove(img)    
+
+    # Construct the output path for the processed image
+    output_path = os.path.join(output_folder, image_file.lower())  
+
+    # Convert the image to 'RGB' mode before saving as JPEG
+    output_image = output_image.convert('RGB')
+
+    # Save the processed image to the output folder
+    output_image.save(output_path)
+
+    img.close()
