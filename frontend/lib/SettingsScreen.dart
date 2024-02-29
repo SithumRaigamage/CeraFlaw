18c9b1555128bbe8734a753f41ff0cf68a443a34
SithumRaigamage <sraig2002@gmail.com>
2024-02-29 17:39:08 +0530

    settingScreen
    updated by putting a banner indicating ''in development stage''

--- frontend/lib/SettingsScreen.dart
+++ frontend/lib/SettingsScreen.dart
@@ -7,18 +7,40 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Settings'),
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/ceraflaw_wallpaper.jpg"),
-            fit: BoxFit.cover,
-            alignment: Alignment.topLeft,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/ceraflaw_wallpaper.jpg"),
+                fit: BoxFit.cover,
+                alignment: Alignment.topLeft,
+              ),
+            ),
+            child: Center(
+              child: Text('Content of Settings Screen'),
+            ),
           ),
-        ),
-        child:Center(
-          child: Text('Content of Settings Screen'),
-        ),
-      )
+          // Diagonal Banner
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.3, // Adjust the height as needed
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              transform: Matrix4.rotationZ(-0.785398),
+              color: Colors.red, // Choose your banner color
+              child: Text(
+                'In Development',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
