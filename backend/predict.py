a2060cc7b4f55eff4cac86dcfee9967e69fefd3f
Thamindu Dasanayake <136196555+thamindu2003@users.noreply.github.com>
2024-03-06 20:47:37 +0530

    Added predict.py and best.pt

---
+++ backend/predict.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+import os
+
+# Load a model
+model = YOLO("best.pt")
+
+# model.predict(source="data/MacTiles/DSC_0589.JPG")
+
+# Set the input and output folders
+input_folder = 'output_images'
+
+# loop through all files in input folder
+for filename in os.listdir(input_folder):
+    if filename.endswith('.JPG') or filename.endswith('.png'):
+        # define input and output file paths
+        input_path = os.path.join(input_folder, filename)
+
+        #model.predict(source="output_images/" + filename)
+        model.predict(source=input_folder + "/" + filename)
+
