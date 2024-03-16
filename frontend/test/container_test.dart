c42f206ce50478e9c24a433f5804cfefb3b0282b
MaddumageTharusha <maddumagedontharusha@gmail.com>
2024-03-17 01:14:58 +0530

    Added container, homescreen, main basic tests

--- frontend/test/container_test.dart
+++ frontend/test/container_test.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ceraflaw/main.dart';
-// import 'package:ceraflaw/HomeScreen.dart';
+
 
 void main() {
   testWidgets('Test CeraFlaw Widget', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(CeraFlaw()); // Create an instance of CeraFlaw widget
 
-    // Verify that the specific Container widget is found
+    // Verify container parameters
     expect(find.byWidgetPredicate((widget) =>
         widget is Container &&
         widget.constraints is BoxConstraints &&
---
+++ frontend/test/main_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ceraflaw/main.dart';
+import 'package:ceraflaw/HomeScreen.dart';
+
+void main() {
+  testWidgets('Test CeraFlaw Widget', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(CeraFlaw()); // Create an instance of CeraFlaw widget
+
+    // Verify bg img
+    expect(find.byType(DecorationImage), findsOneWidget);
+
+    // check availability Homecontent widget
+    expect(find.byType(HomeContent), findsOneWidget);
+  });
+}
