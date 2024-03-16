464ae5427e64188bd715f41d2026af2eed5abf38
MaddumageTharusha <maddumagedontharusha@gmail.com>
2024-03-17 00:33:28 +0530

    added widget test

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -1,34 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ceraflaw/main.dart'; // Import the main.dart file
-import 'package:ceraflaw/HomeScreen.dart'; // Import HomeScreen.dart
+import 'package:ceraflaw/main.dart';
+// import 'package:ceraflaw/HomeScreen.dart';
 
 void main() {
   testWidgets('Test CeraFlaw Widget', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(CeraFlaw()); // Create an instance of CeraFlaw widget
 
-    // Verify that MaterialApp, Scaffold, and HomeContent widgets are present.
-    expect(find.byType(MaterialApp), findsOneWidget);
-    expect(find.byType(Scaffold), findsOneWidget);
-    expect(find.byType(HomeContent), findsOneWidget); // Using HomeContent instead of HomeScreen
-
-    // Verify that the background image is set.
-    expect(find.byType(Container), findsOneWidget);
-    expect(
-      find.descendant(
-        of: find.byType(Container),
-        matching: find.byType(DecorationImage),
-      ),
-      findsOneWidget,
-    );
-
-    // Verify the frame size
-    expect(find.byType(Container), findsOneWidget);
-    expect(
-      tester.getSize(find.byType(Container)),
-      Size(500, 500),
-    );
+    // Verify that the specific Container widget is found
+    expect(find.byWidgetPredicate((widget) =>
+        widget is Container &&
+        widget.constraints is BoxConstraints &&
+        widget.constraints!.maxWidth == 500 &&
+        widget.constraints!.maxHeight == 500), findsOneWidget);
   });
 }
+
 
