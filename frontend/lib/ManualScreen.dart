82aec8f4ba6f2e05cded48fca81a93af1b26fdab
Sithum Raigamage <sraig2002@gmail.com>
2024-03-18 12:23:22 +0530

    notesScreen updated
    updated the noteScreen and add a readme.me  file (reads to the flutter screen) and default template is added to the readme.me

--- frontend/lib/ManualScreen.dart
+++ frontend/lib/ManualScreen.dart
@@ -1,6 +1,21 @@
-import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
+
+import 'dart:async';
 import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ManualScreen(),
+    );
+  }
+}
 
 class ManualScreen extends StatefulWidget {
   @override
@@ -8,127 +23,47 @@ class ManualScreen extends StatefulWidget {
 }
 
 class _ManualScreenState extends State<ManualScreen> {
-  final TextEditingController _notesEditingController = TextEditingController();
-  String _notes = '';
+  String _manualContent = "";
 
   @override
   void initState() {
     super.initState();
-    _loadNotes();
+    loadManual();
   }
 
-  void _loadNotes() async {
+  Future<void> loadManual() async {
     try {
-      final directory = await getApplicationDocumentsDirectory();
-      final file = File('${directory.path}/GitHub/CeraFlaw/notes.txt');
-      if (await file.exists()) {
-        setState(() {
-          _notes = file.readAsStringSync();
-        });
-      }
+      final file = await _localFile;
+      String manualContent = await file.readAsString();
+      setState(() {
+        _manualContent = manualContent;
+      });
     } catch (e) {
-      print("Failed to load notes: $e");
+      print("Error loading manual: $e");
     }
   }
 
-  Future<void> _updateNotes() async {
-    setState(() {
-      _notes += '\n' + _notesEditingController.text;
-      _notesEditingController.clear(); // Clear the input field after appending
-    });
-
-    try {
-      final directory = await getApplicationDocumentsDirectory();
-      final file = File('${directory.path}/GitHub/CeraFlaw/notes.txt');
-      await file.writeAsString(_notes);
-    } catch (e) {
-      print("Failed to update notes: $e");
-    }
-  }
-
-  void _deleteNotes() {
-    setState(() {
-      _notes = '';
-      _notesEditingController.clear();
-    });
-  }
-
-  void _deleteFile() async {
-    try {
-      final directory = await getApplicationDocumentsDirectory();
-      final file = File('${directory.path}/GitHub/CeraFlaw/notes.txt');
-      if (await file.exists()) {
-        await file.delete();
-        setState(() {
-          _notes = ''; // Clear the notes field after deleting the file
-        });
-      }
-    } catch (e) {
-      print("Failed to delete file: $e");
-    }
+  Future<File> get _localFile async {
+    final directory = Directory.current;
+    String filePath = '${directory.path}//README.md'; // Change the file name to README.md
+    return File(filePath);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Notes'),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.delete),
-            onPressed: () {
-              _deleteNotes();
-              _deleteFile(); // Call the function to delete the file
-            },
-          ),
-        ],
+        title: Text('Manual'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      'Notes:',
-                      style: TextStyle(
-                          fontSize: 16.0, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 5),
-                    Text(
-                      _notes,
-                      style: TextStyle(fontSize: 16.0),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              controller: _notesEditingController,
-              decoration: InputDecoration(
-                labelText: 'Enter notes',
-                border: OutlineInputBorder(),
-              ),
-              maxLines: 5,
-            ),
-          ],
+        padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 16.0), // Adjust margin top here
+        child: Center(
+          child: SingleChildScrollView(
+            child: _manualContent.isNotEmpty
+                ? MarkdownBody(data: _manualContent)
+                : CircularProgressIndicator(), // Show loading indicator while content is loading
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _updateNotes, // Call _updateNotes when the button is pressed
-        tooltip: 'Save',
-        child: Icon(Icons.save),
       ),
     );
   }
