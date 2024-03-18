f37f65216a259c4a947ccc94957aa5fda28e4fdd
MaddumageTharusha <maddumagedontharusha@gmail.com>
2024-03-18 16:13:12 +0530

    updated main_test.dart

--- frontend/test/main_test.dart
+++ frontend/test/main_test.dart
@@ -1,4 +1,4 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ceraflaw/main.dart';
 import 'package:ceraflaw/HomeScreen.dart';
@@ -6,7 +6,7 @@ import 'package:ceraflaw/HomeScreen.dart';
 void main() {
   testWidgets('Test main.dart', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(CeraFlaw()); // Create an instance of CeraFlaw widget
+    await tester.pumpWidget(CeraFlaw()); // Create an instance of CeraFlawApp widget
 
     // check availability Homecontent widget
     expect(find.byType(HomeContent), findsOneWidget);
