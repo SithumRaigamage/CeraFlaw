19c57d7de23427a3525eea8ed0559bd6d4cb34e4
MaddumageTharusha <maddumagedontharusha@gmail.com>
2024-03-19 10:16:48 +0530

    minor changes and deleted imports

--- frontend/test/notescreen_test.dart
+++ frontend/test/notescreen_test.dart
@@ -1,5 +1,3 @@
-// import 'dart:convert';
-// import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:ceraflaw/NotesScreen.dart';
@@ -7,39 +5,39 @@ import 'package:ceraflaw/NotesScreen.dart';
 void main() {
   group('NotesScreen tests', () {
     testWidgets('Adding a note', (WidgetTester tester) async {
-      // Build our app and trigger a frame.
+      // build our app and trigger a frame.
       await tester.pumpWidget(MaterialApp(home: NotesScreen()));
 
-      // Fill the text fields with test values.
+      // filling the text fields with test values.
       await tester.enterText(find.byType(TextField).first, 'Test Title');
       await tester.enterText(find.byType(TextField).last, 'Test Message');
 
-      // Tap the add button.
+      // tapping the add button.
       await tester.tap(find.byType(ElevatedButton));
       await tester.pump();
 
-      // Verify that the note is added.
+      // checking if notes got added
       expect(find.text('Test Title'), findsOneWidget);
       expect(find.text('Test Message'), findsOneWidget);
     });
 
     testWidgets('Deleting a note', (WidgetTester tester) async {
-      // Build our app and trigger a frame.
+      // build our app and trigger a frame.
       await tester.pumpWidget(MaterialApp(home: NotesScreen()));
 
-      // Fill the text fields with test values.
+      // fill the text fields with test values.
       await tester.enterText(find.byType(TextField).first, 'Test Title');
       await tester.enterText(find.byType(TextField).last, 'Test Message');
 
-      // Tap the add button.
+      // tapping the add button.
       await tester.tap(find.byType(ElevatedButton));
       await tester.pump();
 
-      // Tap the delete button.
+      // tapping the delete button.
       await tester.tap(find.byIcon(Icons.delete));
       await tester.pump();
 
-      // Verify that the note is deleted.
+      // check if notes get delete
       expect(find.text('Test Title'), findsNothing);
       expect(find.text('Test Message'), findsNothing);
     });
