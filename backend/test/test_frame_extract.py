b598821b2f88cd1ec9d6da338d43aee6ece4b02d
MaddumageTharusha <maddumagedontharusha@gmail.com>
2024-03-20 19:46:11 +0530

    updated test_frame_extract.py

--- backend/test/test_frame_extract.py
+++ backend/test/test_frame_extract.py
@@ -1,30 +1,33 @@
-import cv2
-import unittest
 import os
+import cv2
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  # added parent directory to the python path
+import unittest
 
-from FrameExtract import testExtractFrames, savedata_dir
+# Append parent directory to the Python path to import FrameExtract.py
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# Import the function to be tested
+from FrameExtract import extractFrames, savedata_dir
 
 class TestFrameExtract(unittest.TestCase):
-    def test_frame_extract(self):
-        # save dirct creation
+    def test_extractFrames(self):
+        # Create savedata directory
         os.makedirs(savedata_dir, exist_ok=True)
 
-        # simulate frame capturing
-        testExtractFrames()
+        # Extract frames from test video
+        extractFrames(videoPath=os.path.join(os.path.dirname(__file__), "test_frame_video.mp4"), maxFrames=50)
 
-        # checking if frames were saved
+        # Check if savedata directory exists
         self.assertTrue(os.path.exists(savedata_dir))
 
-        # checking if atleast one frame saved
+        # Check if at least one frame is saved
         frames = os.listdir(savedata_dir)
         self.assertTrue(len(frames) > 0)
 
-        # checking if the files saved was .jpg
+        # Check if the saved files are .jpg
         self.assertTrue(all(file.endswith('.jpg') for file in frames))
 
-        # checking if files can be read as images
+        # Check if files can be read as images
         for file in frames:
             filepath = os.path.join(savedata_dir, file)
             img = cv2.imread(filepath)
@@ -32,7 +35,7 @@ class TestFrameExtract(unittest.TestCase):
             self.assertTrue(img.shape[0] > 0)
             self.assertTrue(img.shape[1] > 0)
 
-        # clean up (remove directory and content)
+        # Clean up (remove directory and content)
         for file in frames:
             os.remove(os.path.join(savedata_dir, file))
         os.rmdir(savedata_dir)
